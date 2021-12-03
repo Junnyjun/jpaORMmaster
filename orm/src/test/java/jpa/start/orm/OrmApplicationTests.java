b8043537802acc57bec725565936ece2f84e6993
Lee-ChangHee <chbe5082@naver.com>
2021-12-03 11:02:02 +0900

    ORM 1st chapter

---
+++ orm/src/test/java/jpa/start/orm/OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.start.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
